9ddc933a7ff12b07012a5d532a6eead646eea5bd
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-09-25 16:26:54 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan15-Logika/src/latihan/pkg15/logika/PBO110118032Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg15.logika;
+
+/**
+ *
+ * @author
+ * NAMA  : Guntur Maulana Ibrahim
+ * Kelas : PBO1
+ * NIM   : 10118032
+ * Deskripsi Program : Program ini berisi untuk menampilkan tipe data logika
+ */
+public class PBO110118032Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a&&b));
+        System.out.println("a || b = " + (a||b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a&&b));
+    }
+    
+}
